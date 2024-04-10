6ed0d58eaa2ae9a5001279c56ffc711a036aa301
rudrakumar2012 <manaw10071994@gmail.com>
2024-04-10 17:43:02 +0530

    Travel Tracker finish

--- 21PostgresSQL/8.3 Travel Tracker/index.js
+++ 21PostgresSQL/8.3 Travel Tracker/index.js
@@ -18,19 +18,57 @@ const port = 3000;
 
 db.connect();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
-app.get("/", async (req, res) => {
+async function checkVisisted() {
   const result = await db.query("SELECT country_code FROM visited_countries");
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
-  console.log(result.rows);
+  return countries;
+}
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  const countries = await checkVisisted();
   res.render("index.ejs", { countries: countries, total: countries.length });
-  db.end();
 });
+
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+  
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%'|| $1 || '%';",
+      [input.toLowerCase()]
+    );
+
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    try {
+      await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [countryCode]);
+      res.redirect("/");
+    } catch (error) {
+      console.log(error);
+      const countries = await checkVisisted();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
+        error: "Country already exists",
+      })      
+    }
+  } catch (error) {
+    console.log(error);
+    const countries = await checkVisisted();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      error: "Country not found",
+    })    
+  }
+
+})
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
