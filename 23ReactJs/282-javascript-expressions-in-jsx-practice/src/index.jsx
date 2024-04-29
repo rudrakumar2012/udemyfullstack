c1a777ab98ccf2d836dc868187c90bb13e45bdf9
rudrakumar2012 <manaw10071994@gmail.com>
2024-04-29 17:52:24 +0530

    JSX Expressions practice

---
+++ 23ReactJs/282-javascript-expressions-in-jsx-practice/src/index.jsx
@@ -0,0 +1,28 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const fName = "Rudra";
+const lName = "Kumar";
+const currentYear = new Date().getFullYear();
+
+ReactDOM.render(
+    <div>
+        <p>Created by {fName + " " + lName}</p>
+        <p>Copyright {currentYear}</p>
+    </div>,
+document.getElementById("root"));
