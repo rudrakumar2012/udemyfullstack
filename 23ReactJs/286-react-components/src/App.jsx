cc6b6429a171f2bbd9e7aa7ac63b54a0fdf341c7
rudrakumar2012 <manaw10071994@gmail.com>
2024-04-30 16:31:28 +0530

    React Components

---
+++ 23ReactJs/286-react-components/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Heading from "./Heading";
+import List from "./List";
+
+export default function App() {
+    return (
+        <div>
+            <Heading />
+            <List />
+        </div>
+    );
+}
