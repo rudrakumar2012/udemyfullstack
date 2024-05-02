90de203caa19f4bf908d41415513009d2073d57d
rudrakumar2012 <manaw10071994@gmail.com>
2024-05-02 17:41:16 +0530

    Mapping Components Practice

---
+++ 23ReactJs/299-mapping-components-practice/src/components/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Emoji from "./Emoji";
+import emojipedia from "../emojipedia";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+        {emojipedia.map((emoji) => (
+          <Emoji
+            key={emoji.id}
+            emoji={emoji.emoji}
+            name={emoji.name}
+            meaning={emoji.meaning}
+          />
+        ))}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
