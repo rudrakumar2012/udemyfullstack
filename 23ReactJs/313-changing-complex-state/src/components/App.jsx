f90c5c18f52f7c265fbdd6c9ebfa6acff13719a8
rudrakumar2012 <manaw10071994@gmail.com>
2024-05-15 16:54:37 +0530

    Changing complex state

---
+++ 23ReactJs/313-changing-complex-state/src/components/App.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+function App() {
+  const [fullName, setFullName] = React.useState({
+    fName: "",
+    lName: "",
+  });
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    setFullName((prevValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value,
+        };
+      }
+    });
+  };
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
+      <form>
+        <input
+          name="fName"
+          placeholder="First Name"
+          onChange={handleChange}
+          value={fullName.fName}
+        />
+        <input
+          name="lName"
+          placeholder="Last Name"
+          onChange={handleChange}
+          value={fullName.lName}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
